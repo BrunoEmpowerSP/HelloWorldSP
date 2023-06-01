9b4b06fe0cd19fa88f918948ab5ff7e468b59685
Bruno Campos <bruno.mendonca@g.globo>
2023-06-01 14:54:21 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld_Empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld_Empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorld_Empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A62AC121-F57E-4FBF-BD11-61BB5FA24C30")]
+[assembly: AssemblyVersion("1.0.0.0")]
